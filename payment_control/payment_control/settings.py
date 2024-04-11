76b40b735bdb1e643de3c5de5490accec0301cfa
Pohrebniak <pohrebniakdanyl@gmail.com>
2024-04-11 17:02:24 +0300

    Hid the secret key

--- payment_control/payment_control/settings.py
+++ payment_control/payment_control/settings.py
@@ -1,6 +1,7 @@
 import os
 
 from pathlib import Path
+from const import SECRETKEY
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -10,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-f54i28n=v0160v3=6e9x%b__@rv+wbyzftrogvre&98x&8po=z'
+SECRET_KEY = SECRETKEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
