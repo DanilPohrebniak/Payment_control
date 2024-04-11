7cfeeba57888a7bef165d1ba1ba0e337d471da29
Pohrebniak <pohrebniakdanyl@gmail.com>
2024-04-11 16:37:11 +0300

    The front-end has been implemented, and minimal functionality has been added.

---
+++ payment_control/pc/views.py
@@ -0,0 +1,65 @@
+from django.shortcuts import render
+from django.views import generic
+from django.utils.safestring import mark_safe
+from datetime import timedelta, datetime, date
+import calendar
+from .utils import Calendar
+from .models import Event
+from .forms import EventForm
+
+
+def get_date(req_day):
+    if req_day:
+        year, month = (int(x) for x in req_day.split("-"))
+        return date(year, month, day=1)
+    return datetime.today()
+
+
+def prev_month(d):
+    first = d.replace(day=1)
+    prev_month = first - timedelta(days=1)
+    month = "month=" + str(prev_month.year) + "-" + str(prev_month.month)
+    return month
+
+
+def next_month(d):
+    days_in_month = calendar.monthrange(d.year, d.month)[1]
+    last = d.replace(day=days_in_month)
+    next_month = last + timedelta(days=1)
+    month = "month=" + str(next_month.year) + "-" + str(next_month.month)
+    return month
+
+
+class CalendarViewNew(generic.ListView):
+    template_name = "calendarapp/calendar.html"
+    form_class = EventForm
+
+    def get(self, request, *args, **kwargs):
+        # request.user
+        forms = self.form_class()
+        events = Event.objects.get_all_events(user=1)
+        events_month = Event.objects.get_running_events(user=1)
+        event_list = []
+        # start: '2020-09-16T16:00:00'
+        for event in events:
+            event_list.append(
+                {
+                    "title": event.title,
+                    "start": event.start_time.strftime("%Y-%m-%dT%H:%M:%S"),
+                    "end": event.end_time.strftime("%Y-%m-%dT%H:%M:%S"),
+
+                }
+            )
+        context = {"form": forms, "events": event_list,
+                   "events_month": events_month}
+        return render(request, self.template_name, context)
+
+    def post(self, request, *args, **kwargs):
+        forms = self.form_class(request.POST)
+        if forms.is_valid():
+            form = forms.save(commit=False)
+            form.user = request.user
+            form.save()
+            return redirect("calendarapp:calendar")
+        context = {"form": forms}
+        return render(request, self.template_name, context)
